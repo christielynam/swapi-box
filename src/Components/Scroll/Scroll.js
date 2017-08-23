7e89331ea1672a5daba05762b20786900d3918ea
Travis Rollins <TROLL59910@aol.com>
2017-08-23 15:39:55 -0600

    Stores favorites.  Can toggle favorites and remove favorites

--- src/Components/Scroll/Scroll.js
+++ src/Components/Scroll/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Scroll.css';
 import Button from '../Button/Button';
 
-const Scroll = ({data, opening}) => {
+const Scroll = ({data, opening, btnFn}) => {
   const text = data[opening].Opening
   const filmTitle = data[opening].Title
   const releaseDate = data[opening].Release
@@ -12,7 +12,7 @@ const Scroll = ({data, opening}) => {
       <div className='crawl-container'>
         <div className='fade'>
           <h1 className='title'>SWAPI-BOX</h1>
-          <Button className={' button favorite'} buttonText='View Favorites' />
+          <Button className={' button favorite'} buttonText='View Favorites' btnFn={btnFn} />
         </div>
         <div className='star-wars'>
           <div className='crawl'>
